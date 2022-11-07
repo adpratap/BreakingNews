1458ef3b49224c10b00a15d7d638016e8c7ace13
Anand Pratap <anandpra7ap@gmail.com>
2022-11-07 13:54:24 +0530

    News Search...

---
+++ app/src/main/java/com/noreplypratap/breakingnews/ui/fragments/NoInternetFragment.kt
@@ -0,0 +1,9 @@
+package com.noreplypratap.breakingnews.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.noreplypratap.breakingnews.R
+
+
+class NoInternetFragment : Fragment(R.layout.fragment_no_internet) {
+
+}
--- app/src/main/java/com/noreplypratap/breakingnews/utils/Constants.kt
+++ app/src/main/java/com/noreplypratap/breakingnews/utils/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val BaseURL = "https://newsapi.org/"
     const val DefaultImageURl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQsq1NacYKHKS-RudSBgbLZa_ndkD-lmmQfA&usqp=CAU"
 
-    const val APIKey = "Use Your API Key"
+    //const val APIKey = "Use Your API Key"
     const val TAG = "MVVMOBreakingNews"
 }
